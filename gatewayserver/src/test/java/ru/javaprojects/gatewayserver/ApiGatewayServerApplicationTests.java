a0974ac11bfdb77d9748bbbd09586417f4f44031
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-01-17 12:31:04 +0300

    Add API Gateway Server

---
+++ gatewayserver/src/test/java/ru/javaprojects/gatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
